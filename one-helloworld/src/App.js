8e25b5f24059ce5d6822531363bc4e6cded76bff
Kushal Desai <kushaldesai1990@gmail.com>
2021-04-19 11:32:22 +0530

    Hello World & Re-usable button component

---
+++ one-helloworld/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+import Button from "./Button";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button title="App Store" />
+      <Button title="Play Store" />
+      <Button />
+      {/* <button className="button">Play Store</button> */}
+    </div>
+  );
+};
+
+export default App;
